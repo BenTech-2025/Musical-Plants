5210335dc9c957277a314e712d150e59fa114c85
BenTech-2025 <myemail@email.com>
2026-01-22 18:13:50 -0800

    Create scripts/cron/ directories and temperature_logger.sh script

---
+++ scripts/cron/temperature_logger.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TIMESTAMP=$(date +%Y-%m-%d_%H:%M)
+TEMPERATURE=$(cat /sys/bus/w1/devices/w1_bus_master1/28-0000005b67ce/temperature)
+
+TEMP_C=$(awk "BEGIN { printf \"%.2f\", $TEMPERATURE / 1000 }")
+
+echo "$TIMESTAMP,$TEMP_C" >> /projects/musical-plants/data/temperature.csv
