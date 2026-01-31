ac739b009a6cc6d61554d3701e0388928bd4f17c
BenTech-2025 <myemail@email.com>
2026-01-30 17:17:42 -0800

    Updated image_capture.sh script to include text overlays

--- scripts/cron/image_capture.sh
+++ scripts/cron/image_capture.sh
@@ -5,17 +5,40 @@ set -eu
 DATE=$(date +%Y-%m-%d)
 MONTHDAY=$(date +%m-%d)
 
+
 mkdir -p "/projects/musical-plants/images/$DATE"
 
+mkdir -p "/projects/musical-plants/videos/$DATE"
+
 for ((i=0; i<96; i++)); do
+
+C02=$(python3 /projects/musical-plants/scripts/python/carbondioxide_ingest.py)
+
+TEMPERATURE=$(cat /sys/bus/w1/devices/w1_bus_master1/28-0000005b67ce/temperature)
+TEMP_C=$(awk "BEGIN { printf \"%.2f\", $TEMPERATURE / 1000 }")
+
+HUMIDITY=$(python3 /projects/musical-plants/scripts/python/humidity_ingest.py)
+
+MOISTURE=$(python3 /projects/musical-plants/scripts/python/soilmoisture_ingest.py)
+
+
     rpicam-still --nopreview --timeout 1000 \
     -o "/projects/musical-plants/images/$DATE/plant_"$MONTHDAY"_$(printf '%04d' "$i").jpg" \
     --awb indoor \
     --gain 1.0 \
     --shutter 16667 \
     --width 4606 \
-    --height 2590
+    --height 2590 && \
+    convert "/projects/musical-plants/images/$DATE/plant_"$MONTHDAY"_$(printf '%04d' "$i").jpg" \
+    -gravity SouthEast \
+    -pointsize 100 \
+    -fill white \
+    -stroke black -strokewidth 1 \
+    -annotate +10+10 "$(date '+%Y-%m-%d %H:%M:%S'), Soil moisture:$MOISTURE, Humidity:$HUMIDITY%, Soil temperature:$TEMP_C°C, C02:$C02 ppm" \
+    "/projects/musical-plants/images/$DATE/plant_"$MONTHDAY"_$(printf '%04d' "$i").jpg"
 
-    sleep 300
+
+    sleep 10
 
 done
+
