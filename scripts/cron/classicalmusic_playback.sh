e70d9a277556b9ef4721c0734e6f303c9f32feeb
BenTech-2025 <myemail@email.com>
2026-02-01 15:30:24 -0800

    Updated classicalmusic_playback.sh to echo a timestamp to the musicdays_log.txt file and other minor adjustments

--- scripts/cron/classicalmusic_playback.sh
+++ scripts/cron/classicalmusic_playback.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -eu
+
+DATE=$(date +%Y-%m-%d-%A)
+
 /usr/bin/cvlc --loop --gain 0.9 /projects/musical-plants/music/classicalmusic_compilation.mp3 &
 
 VLC_PID=$!
 
 sleep 8h
+
+echo "$DATE" >> /projects/musical-plants/data/musicdays_log.txt
 
 kill $VLC_PID
