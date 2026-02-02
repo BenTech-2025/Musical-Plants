83c1e8bb4f3aed25301ae5458bf47b4880c94955
BenTech-2025 <myemail@email.com>
2026-02-01 16:39:05 -0800

    Create logger_orchestration.sh script to prevent overlapping processes and inconsistent timing from cron

---
+++ scripts/cron/logger_orchestration.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+
+/projects/musical-plants/scripts/cron/temperature_logger.sh
+sleep 5
+
+/projects/musical-plants/scripts/cron/humidity_logger.sh
+sleep 5
+
+/projects/musical-plants/scripts/cron/soilmoisture_logger.sh
+sleep 5
+
+/projects/musical-plants/scripts/cron/carbondioxide_logger.sh
