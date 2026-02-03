cc19171fc74717c583d7d446e7377c2a9c832e91
BenTech-2025 <myemail@email.com>
2026-02-02 17:54:13 -0800

    Updated image_capture.sh script to allow failures specifically from sensor reading py scripts. This is to prevent the script from exiting completely again

--- scripts/cron/image_capture.sh
+++ scripts/cron/image_capture.sh
@@ -12,14 +12,14 @@ mkdir -p "/projects/musical-plants/videos/$DATE"
 
 for ((i=0; i<96; i++)); do
 
-C02=$(/projects/musical-plants/venv/bin/python3 /projects/musical-plants/scripts/python/carbondioxide_ingest.py)
+C02=$(/projects/musical-plants/venv/bin/python3 /projects/musical-plants/scripts/python/carbondioxide_ingest.py || echo "NA")
 
 TEMPERATURE=$(cat /sys/bus/w1/devices/w1_bus_master1/28-0000005b67ce/temperature)
 TEMP_C=$(awk "BEGIN { printf \"%.2f\", $TEMPERATURE / 1000 }")
 
-HUMIDITY=$(/projects/musical-plants/venv/bin/python3 /projects/musical-plants/scripts/python/humidity_ingest.py)
+HUMIDITY=$(/projects/musical-plants/venv/bin/python3 /projects/musical-plants/scripts/python/humidity_ingest.py || echo "NA")
 
-MOISTURE=$(/projects/musical-plants/venv/bin/python3 /projects/musical-plants/scripts/python/soilmoisture_ingest.py)
+MOISTURE=$(/projects/musical-plants/venv/bin/python3 /projects/musical-plants/scripts/python/soilmoisture_ingest.py || echo "NA")
 
 
     rpicam-still --nopreview --timeout 1000 \
@@ -34,7 +34,7 @@ MOISTURE=$(/projects/musical-plants/venv/bin/python3 /projects/musical-plants/sc
     -pointsize 100 \
     -fill white \
     -stroke black -strokewidth 1 \
-    -annotate +10+10 "$(date '+%Y-%m-%d %H:%M:%S'), Soil moisture:$MOISTURE, Humidity:$HUMIDITY%, Soil temperature:$TEMP_C°C, C02:$C02 ppm" \
+    -annotate +10+10 "$(date '+%Y-%m-%d %H:%M:%S'), Soil moisture:$MOISTURE, Humidity:$HUMIDITY%, Soil temperature:"${TEMP_C}°C", C02:$C02 ppm" \
     "/projects/musical-plants/images/$DATE/plant_"$MONTHDAY"_$(printf '%04d' "$i").jpg"
 
 
